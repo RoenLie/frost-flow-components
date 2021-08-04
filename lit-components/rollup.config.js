209f0ac8684d313a61edc26932a1d1f3ea172c47
Kristoffer Roen-Lie <kristofferroenlie@gmail.com>
2021-08-05 01:00:19 +0200

    made rollup able to input output multiple bundles.

--- lit-components/rollup.config.js
+++ lit-components/rollup.config.js
@@ -3,59 +3,40 @@ import { terser } from 'rollup-plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 
-export default [
-   {
-      input: './dist/src/components/frost-list-grid/FrostListGrid.js',
-      output: {
-         file: 'build/FrostListGrid.wc.js',
-         format: 'esm',
-      },
-      onwarn(warning) {
-         if (warning.code !== 'THIS_IS_UNDEFINED') {
-            console.error(`(!) ${warning.message}`);
-         }
-      },
-      plugins: [
-         replace({ 'Reflect.decorate': 'undefined' }),
-         resolve(),
-         terser({
-            ecma: 2017,
-            module: true,
-            warnings: true,
-            mangle: {
-               properties: {
-                  regex: /^__/,
-               },
-            },
-         }),
-         summary(),
-      ],
-   },
-   {
-      input: './dist/src/components/frost-list-grid/field-renderers/FrostPreviewOrOpen.js',
-      output: {
-         file: 'build/FrostPreviewOrOpen.wc.js',
-         format: 'esm',
-      },
-      onwarn(warning) {
-         if (warning.code !== 'THIS_IS_UNDEFINED') {
-            console.error(`(!) ${warning.message}`);
-         }
-      },
-      plugins: [
-         replace({ 'Reflect.decorate': 'undefined' }),
-         resolve(),
-         terser({
-            ecma: 2017,
-            module: true,
-            warnings: true,
-            mangle: {
-               properties: {
-                  regex: /^__/,
-               },
-            },
-         }),
-         summary(),
-      ],
-   },
+
+
+const InputOutput = [
+   ['./dist/src/components/frost-list-grid/FrostListGrid.js', 'build/FrostListGrid.wc.js'],
+   ['./dist/src/components/frost-list-grid/field-renderers/FrostPreviewOrOpen.js', 'build/FrostPreviewOrOpen.wc.js']
 ];
+
+
+export default InputOutput.map(inpOut => {
+   return {
+      input: inpOut[0],
+      output: {
+         file: inpOut[1],
+         format: 'esm',
+      },
+      onwarn(warning) {
+         if (warning.code !== 'THIS_IS_UNDEFINED') {
+            console.error(`(!) ${warning.message}`);
+         }
+      },
+      plugins: [
+         replace({ 'Reflect.decorate': 'undefined' }),
+         resolve(),
+         terser({
+            ecma: 2017,
+            module: true,
+            warnings: true,
+            mangle: {
+               properties: {
+                  regex: /^__/,
+               },
+            },
+         }),
+         summary(),
+      ],
+   };
+});
